2f17d56dda7c6190b5b03fb3314cd40561d33a78
raphaella-rose <raphaella.rose1@gmail.com>
2022-08-31 16:24:37 +0100

    index function logs all todos

--- backend/controllers/todo.js
+++ backend/controllers/todo.js
@@ -1,6 +1,15 @@
 const Todo = require("../models/todo");
 
 const TodoController = {
+    index: async (req, res) => {
+      console.log("test");
+      Todo.find().exec((err, allTodos) => {
+        if (err) {
+          throw err
+        }
+        console.log(allTodos)
+      })
+    },
     create: async (req, res) => {
       const todo = new Todo({title: req.body.title, content: req.body.content});
       await todo.save().then(() => res.status(201).json(todo));
--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const TodoController = require("../controllers/todo");
 
+router.get("/", TodoController.index);
 router.post("/create", TodoController.create);
 
 module.exports = router;
