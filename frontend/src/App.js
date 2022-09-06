368a87f173f0fa851043eed08865ea4792d917a3
raphaella-rose <raphaella.rose1@gmail.com>
2022-09-06 10:20:44 +0100

    console logs todo  added when form  submits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,36 @@
-import TodoList from "./TodoList";
+import React from "react"
 
-const dummyTodos = [
-  {
-    title: "Wash the dog",
-    content: "Strawberry shampoo"
-  },
-  {
-    title: "Walk the dog",
-    content: "in Richmond Park"
-  },
-  {
-    title: "Feed the dog",
-    content: "chicken"
-  },
-]
-  
 
+export const addTodo = async (todo) => {
+  try {
+    const res = await fetch('http://localhost:8080/todo/create', {
+      method: "POST",
+      body: JSON.stringify({
+        title: todo.title,
+        content: todo.content
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      },
+    });
+  } catch (err) {}
+}
+
+const updateTodos = () => {
+  addTodo({title: "Feed the dog", content: "chicken"}) //apiFunction
+  .then(() => console.log("todo added"))
+  .catch((err) => console.log(err));
+}
 
 function App() {
+  updateTodos()
   return (
     <div className="App">
       <form>
-        <input type="text" value="" placeholder="title" name="title" />
-        <input type="text" value="" placeholder="content" name="content" />
+        <input type="text" placeholder="title" name="title" />
+        <input type="text" placeholder="content" name="content" />
         <input type="submit" value="submit" />
       </form>
-      <TodoList todos={dummyTodos} />
     </div>
   );
 }
--- frontend/src/TodoList.js
+++ frontend/src/TodoList.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from "react";
 import Todo from './Todo';
 
+
 const TodoList = ({ todos }) => {
+  console.log("todos")
   return todos.map((todo) => {
     return (
       <Todo todo={todo} />
